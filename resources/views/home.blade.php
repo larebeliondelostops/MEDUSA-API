95d8df0de342d180651d7fac10fa03cb69610cc4
JCGarcia25 <johan.caicedo.garcia@unillanos.edu.co>
2023-06-13 17:24:10 -0500

    botones para la activación y removida de las escrituras sobre el mapa

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,11 +22,22 @@
                 </div>
             </div>
         </div>
+
+        <div id="floating-panel">
+            <button id="show-geojson" class="btn btn-primary">
+                <i class="fas fa-eye"></i> Show GeoJSON
+            </button>
+            <button id="hide-geojson" class="btn btn-primary">
+                <i class="fas fa-eye-slash"></i> Hide GeoJSON
+            </button>
+        </div>
     </section>
 
     <!-- Define la función de inicialización del mapa -->
     <script>
         let map;
+        let geoJsonFeatures;
+        let polygon;
         // Transformar el GeoJSON a un objeto JavaScript
         let geoJson = JSON.parse(@json($geoJsonGeometry));
         
@@ -36,17 +47,38 @@
                 zoom: 14,
             });
             map.data.loadGeoJson('js/Cabeceras.json');
-            // Procesa el polígono
-            if (geoJson.type === 'Polygon') {
-                // Crea un nuevo polígono de Google Maps
-                let polygon = new google.maps.Polygon({
-                    paths: geoJson.coordinates[0].map(function(coord) {
-                        // Recuerda que Google Maps usa la estructura {lat: Number, lng: Number}, y el orden de las coordenadas es latitud, luego longitud
-                        return {lat: coord[1], lng: coord[0]};
-                    }),
-                    map: map,
-                });
-            }
+
+            // Crea un nuevo polígono de Google Maps
+            let polygon = new google.maps.Polygon({
+                paths: geoJson.coordinates[0].map(function(coord) {
+                    // Recuerda que Google Maps usa la estructura {lat: Number, lng: Number}, y el orden de las coordenadas es latitud, luego longitud
+                    return {lat: coord[1], lng: coord[0]};
+                }),
+                map: map,
+            });
+
+            // Crear los elementos de control (botones)
+            const showGeoJsonButton = document.getElementById('show-geojson');
+            const hideGeoJsonButton = document.getElementById('hide-geojson');
+
+            // Crear una nueva div para contener los elementos de control
+            const controlDiv = document.createElement('div');
+            controlDiv.style.margin = '10px';
+            controlDiv.appendChild(showGeoJsonButton);
+            controlDiv.appendChild(hideGeoJsonButton);
+
+            // Añadir la div de control al mapa
+            map.controls[google.maps.ControlPosition.TOP_CENTER].push(controlDiv);
+
+            // Añadir funcionalidad a los botones
+            showGeoJsonButton.addEventListener('click', () => {
+                polygon.setMap(map);
+                map.data.setMap(map);
+            });
+            hideGeoJsonButton.addEventListener('click', () => {
+                polygon.setMap(null);
+                map.data.setMap(null);
+            });
         }
     </script>
 

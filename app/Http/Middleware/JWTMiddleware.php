245c50d0b58a82eeb1ee6596f954d2023f3f2c6d
JCGarcia25 <johan.caicedo.garcia@unillanos.edu.co>
2023-07-08 20:15:45 -0500

    Documentación y ajuste en las validaciones de los métodos con su respectivo custom request

--- app/Http/Middleware/JWTMiddleware.php
+++ app/Http/Middleware/JWTMiddleware.php
@@ -4,8 +4,9 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
 
 class JWTMiddleware
 {
@@ -20,11 +21,19 @@ class JWTMiddleware
     {
         try {
 
+            // Verficar si existe un token en el header
             $token = $request->header('Authorization');
             if (!$token) {
                 return response()->json(['message' => 'No se proporcionó un token'], 401);
             }
 
+            // Verificar si el token ha expirado
+            /* dd(JWTAuth::check(substr($token, 7)));
+            if (!JWTAuth::check(substr($token, 7))) {
+                throw new TokenExpiredException('Token expirado');
+            } */
+
+            // Verificar que el usuario con este token exista en la base de datos
             $user = JWTAuth::parseToken()->authenticate();
             if (!$user) {
                 return response()->json(['message' => 'Usuario no encontrado'], 500);

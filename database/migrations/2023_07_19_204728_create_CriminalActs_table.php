2971c58899714c7aab34ad2603c3ffc5c3ecf1cb
AlexDaVinci <dacostaojeda2000@gmail.com>
2023-07-19 16:00:16 -0500

    se crearon las migraciones de modelo probabilistic

---
+++ database/migrations/2023_07_19_204728_create_CriminalActs_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('CriminalActs', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('IndicatorId');
+            $table->foreign('IndicatorId')->references('id')->on('Indicators');
+            $table->bigInteger('ProbabilisticGridId');
+            $table->foreign('ProbabilisticGridId')->references('id')->on('ProbabilisticGrid');
+            $table->string('head')->nullable();
+            $table->string('district')->nullable();
+            $table->string('station')->nullable();
+            $table->string('cai')->nullable();
+            $table->string('quadrant')->nullable();
+            $table->string('neighborhood')->nullable();
+            $table->integer('number')->nullable();
+            $table->string('address')->nullable();
+            $table->string('site')->nullable();
+            $table->string('month')->nullable();
+            $table->string('week')->nullable();
+            $table->string('date')->nullable();
+            $table->string('day')->nullable();
+            $table->string('time')->nullable();
+            $table->string('hour_24')->nullable();
+            $table->string('crime')->nullable();
+            $table->string('conduct')->nullable();
+            $table->string('modality')->nullable();
+            $table->string('description')->nullable();
+            $table->string('asset_class')->nullable();
+            $table->string('model')->nullable();
+            $table->string('zone')->nullable();
+            $table->string('unique_number')->nullable();
+            $table->string('quantity_2')->nullable();
+            $table->string('plate')->nullable();
+            $table->json('coordinates')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('CriminalActs');
+    }
+};

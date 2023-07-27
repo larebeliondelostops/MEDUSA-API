3477f8e24d874348282a0b7c1eff000bbda8774a
AlexDaVinci <dacostaojeda2000@gmail.com>
2023-07-27 10:39:29 -0500

    migracion y modelo para incidentes

---
+++ database/migrations/2023_07_27_152704_create_incident_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incident', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('IndicatorId');
+            $table->foreign('IndicatorId')->references('id')->on('Indicators');
+            $table->string('address');
+            $table->text('description');
+            $table->json('pointCoordinates');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('incident', function (Blueprint $table) {
+            // Eliminar la clave foránea 'IndicatorId'
+            $table->dropForeign(['IndicatorId']);
+        });
+        Schema::dropIfExists('incident');
+    }
+};

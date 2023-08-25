483b0306db520fb94f917725bed24d2b32a0bbfa
JCGarcia25 <johan.caicedo.garcia@unillanos.edu.co>
2023-08-24 21:24:29 -0500

    Campos necesarios para el modelo Users

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,9 @@ class User extends Authenticatable implements JWTSubject
     protected $fillable = [
         'name',
         'email',
+        'phone_number',
+        'address',
+        'avatar',
         'password',
     ];
 

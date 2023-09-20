53f031dbe5761cb6a5a53144a4c5d0b252e80e80
JCGarcia25 <johan.caicedo.garcia@unillanos.edu.co>
2023-09-19 19:09:24 -0500

    CRUD generico

---
+++ app/Http/Controllers/CRUDController.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use App\Models\Marker;
+use App\Contexts\AllDataContext;
+use App\Models\Slug;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Response;
+
+class CRUDController extends Controller
+{
+    /**
+     * Variable para almacenar el contexto de la data
+     */
+    private $value;
+    private $all_data;
+    private $slugs;
+
+    /**
+     * AllDataController constructor.
+     */
+    public function __construct()
+    {
+        $this->value = AllDataContext::VALUE[config("database.default")];
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function all($slug)
+    {
+        try {
+
+            $this->slugs = Slug::where('name', $slug)->first();
+
+            $all_data = $this->value::STRATEGY[$this->slugs->id]::all();
+
+            return Response::json($all_data, 200, [], JSON_PRETTY_PRINT);
+        } catch (Exception $exception) {
+            Log::error($exception->getMessage() . ' - ' . $exception->getLine() . ' - ' . $exception->getFile());
+            return Response::json([
+                'code' => '1001',
+                'status' => 'error',
+                'message' => 'Error En La Generación De La Solicitud'
+            ], 500, [], JSON_PRETTY_PRINT);
+        }
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function allTable($slug)
+    {
+        try {
+            $this->slugs = Slug::where('name', $slug)->first();
+
+            $strategy = $this->value::STRATEGY[$this->slugs->id];
+            $strategy = new $strategy();
+            $data = $strategy->allTable();
+
+            return Response::json($data, 200, [], JSON_PRETTY_PRINT);
+        } catch (Exception $exception) {
+            Log::error($exception->getMessage() . ' - ' . $exception->getLine() . ' - ' . $exception->getFile());
+            return Response::json([
+                'code' => '1001',
+                'status' => 'error',
+                'message' => 'Error En La Generación De La Solicitud'
+            ], 500, [], JSON_PRETTY_PRINT);
+        }
+    }
+
+    public function get($slug, $id)
+    {
+        try {
+            $this->slugs = Slug::where('name', $slug)->first();
+
+            $strategy = $this->value::STRATEGY[$this->slugs->id];
+            $strategy = new $strategy();
+            $data = $strategy->get($id);
+
+            return Response::json($data, 200, [], JSON_PRETTY_PRINT);
+        } catch (Exception $exception) {
+            Log::error($exception->getMessage() . ' - ' . $exception->getLine() . ' - ' . $exception->getFile());
+            return Response::json([
+                'code' => '1001',
+                'status' => 'error',
+                'message' => 'Error En La Generación De La Solicitud'
+            ], 500, [], JSON_PRETTY_PRINT);
+        }
+    }
+
+    public function store(Request $request, $slug)
+    {
+        try {
+            $this->slugs = Slug::where('name', $slug)->first();
+
+            $strategy = $this->value::STRATEGY[$this->slugs->id];
+            $strategy = new $strategy();
+            $data = $strategy->store($request->all());
+
+            return Response::json($data, 200, [], JSON_PRETTY_PRINT);
+        } catch (Exception $exception) {
+            Log::error($exception->getMessage() . ' - ' . $exception->getLine() . ' - ' . $exception->getFile());
+            return Response::json([
+                'code' => '1001',
+                'status' => 'error',
+                'message' => 'Error En La Generación De La Solicitud'
+            ], 500, [], JSON_PRETTY_PRINT);
+        }
+    }
+
+    public function update(Request $request, $slug, $id)
+    {
+        try {
+            $this->slugs = Slug::where('name', $slug)->first();
+
+            $strategy = $this->value::STRATEGY[$this->slugs->id];
+            $strategy = new $strategy();
+            $data = $strategy->update($request->all());
+
+            return Response::json($data, 200, [], JSON_PRETTY_PRINT);
+        } catch (Exception $exception) {
+            Log::error($exception->getMessage() . ' - ' . $exception->getLine() . ' - ' . $exception->getFile());
+            return Response::json([
+                'code' => '1001',
+                'status' => 'error',
+                'message' => 'Error En La Generación De La Solicitud'
+            ], 500, [], JSON_PRETTY_PRINT);
+        }
+    }
+
+    public function destroy($slug, $id)
+    {
+        try {
+            $this->slugs = Slug::where('name', $slug)->first();
+
+            $strategy = $this->value::STRATEGY[$this->slugs->id];
+            $strategy = new $strategy();
+            $data = $strategy->destroy($id);
+
+            return Response::json($data, 200, [], JSON_PRETTY_PRINT);
+        } catch (Exception $exception) {
+            Log::error($exception->getMessage() . ' - ' . $exception->getLine() . ' - ' . $exception->getFile());
+            return Response::json([
+                'code' => '1001',
+                'status' => 'error',
+                'message' => 'Error En La Generación De La Solicitud'
+            ], 500, [], JSON_PRETTY_PRINT);
+        }
+    }
+}
---
+++ database/seeders/SlugsSeeder.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class SlugsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /**
+         * Marcadores para villavicencio
+         */
+        DB::connection('villavicencio')->table('slugs')->insert([
+            [
+                'id' => 1,
+                'name' => 'alarm',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+              'id' => 2,
+              'name' => 'cai',
+              'created_at' => now(),
+              'updated_at' => now(),
+            ],
+            [
+              'id' => 3,
+              'name' => 'entity',
+              'created_at' => now(),
+              'updated_at' => now(),
+            ],
+            [
+              'id' => 4,
+              'name' => 'pollingPlace',
+              'created_at' => now(),
+              'updated_at' => now(),
+            ],
+            [
+              'id' => 50,
+              'name' => 'camera',
+              'created_at' => now(),
+              'updated_at' => now(),
+            ],
+            [
+              'id' => 54,
+              'name' => 'movementUnity',
+              'created_at' => now(),
+              'updated_at' => now(),
+            ],
+            [
+              'id' => 55,
+              'name' => 'event',
+              'created_at' => now(),
+              'updated_at' => now(),
+            ],
+        ]);
+
+        /**
+         * Marcadores para neiva
+         */
+        DB::connection('neiva')->table('slugs')->insert([
+            [
+              'id' => 4,
+              'name' => 'ambient',
+              'created_at' => now(),
+              'updated_at' => now(),
+            ],
+        ]);
+    }
+}

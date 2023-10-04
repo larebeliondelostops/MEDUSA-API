ce30f472859f0330e68a2b6f3ccea67c8b66c321
danielxz331 <danielxz331@gmail.com>
2023-10-04 01:43:11 +0000

    ajuste en las coordenadas de eventos

--- app/Strategies/GetEvents/GetEventCoordinate.php
+++ app/Strategies/GetEvents/GetEventCoordinate.php
@@ -83,7 +83,9 @@ class GetEventCoordinate implements GetEventInterface
     public function OrderEvents($events)
     {
         $eventosOrganizados = $events->map(function ($evento) {
+
             return [
+                'markerType' => 55,
                 'properties' => [
                     'ID' => $evento->id,
                     'idEventType' => $evento->idEventType,
@@ -97,7 +99,7 @@ class GetEventCoordinate implements GetEventInterface
                 ],
                 'position' => [
                     'type' => 'polygon',
-                    'coordinates' => $evento['eventCoordinate']->pointCoordinates
+                    'coordinates' => json_decode($evento['eventCoordinate']->pointCoordinates)->coordinates
                 ]
             ];
         });

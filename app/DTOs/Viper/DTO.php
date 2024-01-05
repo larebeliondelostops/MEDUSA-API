af01d8152c27dc97fe9a7b69ea03c3b40a078974
JorgeGorrito <j0rg3.4b3ll4@gmail.com>
2024-01-05 15:50:45 -0500

    Se agregaron nuevas funcionalidades para facilitar la comunicacion en json y con los modelos

--- app/DTOs/Viper/DTO.php
+++ app/DTOs/Viper/DTO.php
@@ -6,6 +6,33 @@ use \ReflectionClass;
 
 class DTO 
 {
+    public function __construct(array $data) {
+        foreach ($data as $key => $value) {
+            if (property_exists($this, $key)) {
+                $this->{$key} = $value;
+            }
+        }
+    }
+
+    private function snakeToCamelCase($string)
+    {
+        $str = str_replace(' ', '', ucwords(str_replace('_', ' ', $string)));
+        $str[0] = strtolower($str[0]);
+        return $str;
+    }
+
+    public function toArrayLowerCase()
+    {
+        $array = [];
+        $reflectionClass = new ReflectionClass($this);
+        foreach ($reflectionClass->getProperties() as $property) {
+            $property->setAccessible(true);
+            $camelCaseKey = $this->snakeToCamelCase($property->getName());
+            $array[$camelCaseKey] = $property->getValue($this);
+        }
+        return $array;
+    }
+
     public function toArray()
     {
         $array = [];

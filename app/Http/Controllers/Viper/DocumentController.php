2c1667309355a6ac8ff2761e0da90bf44848e359
Daniel Alferez <alferezgarciadanielcamilo@gmail.com>
2024-01-11 01:50:13 -0500

    Se mejoro la documentacion para Viper-Documents-Folder v1.0.0

--- app/Http/Controllers/Viper/DocumentController.php
+++ app/Http/Controllers/Viper/DocumentController.php
@@ -8,6 +8,17 @@ use App\Interfaces\Viper\DocumentInterface;
 use App\DTOs\Viper\DocumentDTO;
 use Storage;
 
+/**
+ * Controlador que maneja todo lo que tiene que ver con las los documentos almacenados en spaces de Digital Ocean
+ *
+ * Controlador que maneja la logica para la creacion, actualizacion, eliminacion y consulta de los documentos en los proyectos de Viper
+ *
+ * @package    App\Http\Controllers\Viper
+ * @copyright  2024 Ignicion S.A.S.
+ * @author     Daniel Alferez <dan.alferez1@gmail.com>
+ * @version    v1.0.0
+ */
+
 class DocumentController extends Controller
 {
     private DocumentInterface $documentInterface;
@@ -18,91 +29,135 @@ class DocumentController extends Controller
     }
 
     /**
-     * Display a listing of the resource.
+     * Mostrar una lista de los recursos.
      *
      * @return \Illuminate\Http\Response
     */
     public function index()
     {
-        // Obtener la lista de archivos en la carpeta 'test'
-        $documents = $this->documentInterface->getAllDocuments();
-
-        return response()->json(['documents' => $documents]);
+        try {
+            // Obtener la lista de documentos.
+            $documents = $this->documentInterface->getAllDocuments();
+    
+            return response()->json(['data' => $documents]);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Almacenar un nuevo recurso en el almacenamiento.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
     */
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
-            'file' => 'required|file',
-            'project_id' => 'required|integer',
-            'folder_id' => 'required|integer',
-            'responsible' => 'required|string',
-        ]);
+        try {
+            $validatedData = $request->validate([
+                'files.*' => 'required|file',
+                'project_id' => 'required|integer',
+                'folder_id' => 'required|string',
+                'responsible' => 'required|string',
+            ]);
 
-        $folderId = (int)$validatedData['folder_id'];
-        $projectId = (int)$validatedData['project_id'];
-        
-        // Crear el DTO
-        $documentDTO = new DocumentDTO(
-            '',
-            '',
-            $validatedData['responsible'],
-            $folderId,
-        );
-    
-        $result = $this->documentInterface->createNewDocument($documentDTO, $validatedData['file'], $validatedData['project_id']);
-    
-        return response()->json($result, 201);
+            $folderId = (int)$validatedData['folder_id'];
+
+            $uploadedFiles = $request->file('files');
+
+            $results = [];
+
+            foreach ($uploadedFiles as $file) {
+                // Crear un DTO para cada archivo.
+                $documentDTO = new DocumentDTO(
+                    '',
+                    '',
+                    $validatedData['responsible'],
+                    $folderId
+                );
+
+                // Crear y almacenar cada documento.
+                $result = $this->documentInterface->createNewDocument($documentDTO, $file, $validatedData['project_id']);
+                $results[] = $result;
+            }
+
+            return response()->json(['data' => $results], 201);
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            return response()->json(['error' => $e->errors()], 422);
+        } catch (\Exception $e) {
+            if ($e->getCode() === 404) {
+                return response()->json(['error' => $e->getMessage()], 404);
+            }
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 
     /**
-     * Update the name of a specified document.
+     * Actualizar el nombre de un documento especificado.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  int  $documentId
      * @return \Illuminate\Http\Response
     */
     public function update(Request $request, int $documentId)
     {
-        $request->validate([
-            'new_name' => 'required|string',
-        ]);
-    
-        // Obtener el nuevo nombre desde la solicitud
-        $newName = $request->input('new_name');
-    
-        $result = $this->documentInterface->updateDocument($documentId, $newName);
-    
-        // Retornar la respuesta según lo que devuelva el servicio
-        return response()->json($result);
+        try {
+            $request->validate([
+                'new_name' => 'required|string',
+            ]);
+        
+            // Obtener el nuevo nombre desde la solicitud.
+            $newName = $request->input('new_name');
+        
+            $result = $this->documentInterface->updateDocument($documentId, $newName);
+        
+            // Retornar la respuesta según lo que devuelva el servicio.
+            return response()->json($result, 200);
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            return response()->json(['error' => $e->errors()], 422);
+        } catch (\Exception $e) {
+            if ($e->getCode() === 404) {
+                return response()->json(['error' => $e->getMessage()], 404);
+            }
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 
-
     /**
-     * Remove the specified resource from storage.
+     * Eliminar el recurso especificado del almacenamiento.
      *
-     * @param  int  $id
+     * @param  int  $documentId
      * @return \Illuminate\Http\Response
     */
     public function destroy($documentId)
     {
-        // Obtener la ruta del archivo antes de eliminarlo
-        $document = $this->documentInterface->deleteDocument($documentId);
-        return  response()->json($document, 200);
+        try {
+            // Obtener la ruta del archivo antes de eliminarlo.
+            $document = $this->documentInterface->deleteDocument($documentId);
+            return  response()->json($document, 200);
+        } catch (\Exception $e) {
+            if ($e->getCode() === 404) {
+                return response()->json(['error' => $e->getMessage()], 404);
+            }
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 
-
+    /**
+     * Obtener todos los documentos en los espacios de almacenamiento.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function allSpaces()
     {
-        $documents = $this->documentInterface->listDocumentsInSpaces('test');
-        return response()->json(['documents' => $documents]);
+        try {
+            $documents = $this->documentInterface->listDocumentsInSpaces('test');
+            return response()->json(['data' => $documents]);
+        } catch (\Exception $e) {
+            if ($e->getCode() === 404) {
+                return response()->json(['error' => $e->getMessage()], 404);
+            }
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
-
-    
 }
--- app/Interfaces/Viper/FolderInterface.php
+++ app/Interfaces/Viper/FolderInterface.php
@@ -1,13 +1,65 @@
-<?php 
+<?php
 
 namespace App\Interfaces\Viper;
 
 use App\DTOs\Viper\FolderDTO;
 
+/**
+ * Interface FolderInterface
+ *
+ * Esta interfaz define los métodos que deben ser implementados por cualquier clase que actúe como servicio
+ * para la manipulación de carpetas en el sistema Viper.
+ * @package    App\Service\Viper
+ * @author     Daniel Alférez <dan.alferez1@gmail.com>
+ * @copyright  2024 Ignicion S.A.S.
+ * @version    v1.0.0
+ */
+
 interface FolderInterface {
-    public function createNewFolder(FolderDTO $folderDTO);
+    
+    /**
+     * Crea una nueva carpeta en el sistema Viper.
+     *
+     * @param FolderDTO $folderDTO Datos de la carpeta a crear.
+     * @param int $higherFolderId Identificador de la carpeta padre (si tiene)
+     */
+    public function createNewFolder(FolderDTO $folderDTO, int $higherFolderId);
+
+    /**
+     * Obtiene todas las carpetas asociadas a un proyecto y su jerarquía.
+     *
+     * @param int $projectId Identificador (bpin) del proyecto
+     */
     public function getAllFolders(int $projectId);
+
+    /**
+     * Obtiene la información detallada de una carpeta específica en el sistema Viper.
+     *
+     * @param int $folderId Identificador único de la carpeta.
+     */
     public function getFolder(int $folderId);
+
+    /**
+     * Actualiza el nombre de una carpeta específica en el sistema Viper.
+     *
+     * @param int $folderId Identificador único de la carpeta a actualizar.
+     * @param string $newName Nuevo nombre para la carpeta.
+     */
     public function updateFolderName(int $folderId, string $newName);
+
+    /**
+     * Elimina una carpeta específica y todas sus subcarpetas en el sistema Viper.
+     *
+     * @param int $folderId Identificador único de la carpeta a eliminar.
+     */
     public function deleteFolder(int $folderId);
+
+    /**
+     * Crea una jerarquía de carpetas en el sistema Viper, incluyendo subcarpetas según la estructura proporcionada.
+     *
+     * @param array $folderData Datos de la carpeta principal y sus subcarpetas.
+     * @param int $projectId Identificador del proyecto al que pertenecen las carpetas.
+     * @param array|null $parentFolder Datos de la carpeta superior (opcional) para establecer relaciones jerárquicas.
+     */
+    public function createFolderHierarchy($folderData, $projectId, $parentFolder = null);
 }

607e4194274b24036dba03555d7b49af6d59edb5
JorgeGorrito <j0rg3.4b3ll4@gmail.com>
2024-01-13 16:42:56 -0500

    Endpoint para departamento contruido -- sin documentar

---
+++ app/Http/Controllers/Viper/DepartmentController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers\Viper;
+
+use App\DTOs\Viper\Department\DepartmentDTO;
+use App\Http\Request\Viper\DepartmentRequest;
+use App\Interfaces\Viper\DepartmentInterface;
+
+use Exception;
+use Illuminate\Http\Request;
+
+class DepartmentController extends BaseController
+{
+    private DepartmentInterface $departmentInterface;
+
+    public function __construct(DepartmentInterface $departmentInterface)
+    {
+        parent::__construct();
+        $this->departmentInterface = $departmentInterface;
+    }
+
+    public function store(DepartmentRequest $request)
+    {
+        try
+        {
+            $data = $request->validated();
+            $newDepartmentDTO = new DepartmentDTO($data);
+            $departmentSaved = $this->departmentInterface->createNewDepartment($newDepartmentDTO);
+            return response()->json([
+                "message" => "Departamento creado satisfactoriamente.",
+                "data" => $departmentSaved
+            ]);
+        }
+        catch(Exception $exception)
+        {
+            return $this->handleException($exception);
+        }
+    }
+
+    public function index(Request $request)
+    {
+        try
+        {
+            return response()->json([
+                "data"=> $this->departmentInterface->getAllDepartments(),
+            ]);
+        }
+        catch(Exception $exception)
+        {
+            return $this->handleException($exception);
+        }
+    }
+
+    public function show(Request $request, int $id)
+    {
+        try
+        {
+            return response()->json([
+                "data" => $this->departmentInterface->getDepartmentById($id),
+            ]);
+        }
+        catch(Exception $exception)
+        {
+            return $this->handleException($exception);
+        }
+    }
+
+    public function update(DepartmentRequest $request, int $id)
+    {
+        try
+        {
+            $data = $request->validated();
+            $departmentForUpdate = new DepartmentDTO($data);
+            $departmentUpdated = $this->departmentInterface->updateDepartment($departmentForUpdate, $id);
+            return response()->json([
+                "message" => "Departamento actualizado satisfactoriamente",
+                "data" => $departmentUpdated,
+            ]);
+        }
+        catch(Exception $exception)
+        {
+            return $this->handleException($exception);
+        }
+    }
+
+    public function destroy(Request $request, int $id)
+    {
+        try
+        {
+            $departmentDeleted = $this->departmentInterface->deleteDepartment($id);
+            return response()->json([
+                "message" => "Departamento eliminado satisfactoriamente.",
+                "data"=> $departmentDeleted,
+            ]);
+        }
+        catch(Exception $exception)
+        {
+            return $this->handleException($exception);
+        }
+    }
+}

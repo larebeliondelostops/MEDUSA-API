8a7ffc95f648dabfb7d8df480f4a3e06ea40ad13
Daniel Alferez <alferezgarciadanielcamilo@gmail.com>
2024-01-14 21:17:18 -0500

    Endpoint (get) Viper-Selects v1.0

---
+++ app/Http/Controllers/Viper/SelectsController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Viper;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Interfaces\Viper\SelectsInterface;
+
+/**
+ * Controlador que maneja todo lo que tiene que ver con los selects para la creación de un proyecto
+ *
+ * @package    App\Http\Controllers\Viper
+ * @copyright  2024 Ignicion S.A.S.
+ * @author     Daniel Alferez <dan.alferez1@gmail.com>
+ * @version    v1.0.0
+ */
+
+class SelectsController extends BaseController
+{
+    private SelectsInterface $selectsInterface;
+
+    public function __construct(SelectsInterface $selectsInterface)
+    {
+        parent::__construct(); // Se tiene que llamar al contructor padre para que se configure correctamente el BaseController
+        $this->selectsInterface = $selectsInterface;
+    }
+
+     /**
+     * Mostrar los datos disponibles en los diferentes selects para la creación de un proyecto.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        try {
+            $selectss = $this->selectsInterface->getAllSelects();
+
+            return response()->json([
+                'data' => $selectss,
+            ], 200);
+        } catch (\Exception $exception) {
+            return $this->handleException($exception);
+        }
+    }
+
+}

8b585f63d55e3f807960c13de58eb71aa0f0b62a
JorgeGorrito <j0rg3.4b3ll4@gmail.com>
2024-01-17 15:09:37 -0500

    Se documentos las todas las capas de estados

--- app/Http/Controllers/Viper/StateController.php
+++ app/Http/Controllers/Viper/StateController.php
@@ -9,20 +9,45 @@ use App\Interfaces\Viper\StateInterface;
 
 // Librerias de terceros
 use Exception;
-use PDOException;
 use Illuminate\Http\Request;
 
+/**
+ * Controlador para la gestión de estados del módulo VIPER
+ *
+ * Este controlador gestiona las operaciones relacionadas con los estados que un proyecto puede tener,
+ * incluyendo la creación, actualización, recuperación y eliminación de estados.
+ *
+ * @package    App\Http\Controllers\Viper
+ * @copyright  2024 Ignicion S.A.S.
+ * @author     Jorge Abella <j0rg3.4b3ll4@gmail.com>
+ * @version    v1.0.2
+ */
 class StateController extends BaseController
 {
     private StateInterface $stateInterface;
 
+    /**
+     * Constructor del controlador.
+     *
+     * Inicializa la interfaz de estado, que es inyectada a través de la inyección de dependencias.
+     *
+     * @param StateInterface $stateInterface Interfaz para la lógica de negocio de estados.
+     */
     public function __construct(StateInterface $stateInterface)
     {
         parent::__construct();
         $this->stateInterface = $stateInterface;
     }
 
-
+    /**
+     * Almacena un nuevo estado en la base de datos.
+     *
+     * Valida los datos de la solicitud y crea un nuevo estado mediante la interfaz StateInterface.
+     * Retorna una respuesta JSON con el estado creado.
+     *
+     * @param StateRequest $request Datos validados de la solicitud para crear un estado.
+     * @return \Illuminate\Http\JsonResponse Respuesta JSON del estado creado.
+     */
     public function store(StateRequest $request)
     {
         try
@@ -40,6 +65,17 @@ class StateController extends BaseController
             return $this->handleException($exception);
         }
     }
+
+    /**
+     * Actualiza un estado existente.
+     *
+     * Valida y actualiza los datos de un estado específico identificado por su ID.
+     * Retorna una respuesta JSON con los datos del estado actualizado.
+     *
+     * @param StateRequest $request Datos validados de la solicitud para actualizar un estado.
+     * @param int $id Identificador del estado a actualizar.
+     * @return \Illuminate\Http\JsonResponse Respuesta JSON del estado actualizado.
+     */
     public function update(StateRequest $request, int $id)
     {
         try
@@ -60,6 +96,14 @@ class StateController extends BaseController
         }
     }
 
+    /**
+     * Muestra los detalles de un estado específico.
+     *
+     * Recupera y retorna los detalles de un estado dado su ID.
+     *
+     * @param int $id Identificador del estado a mostrar.
+     * @return \Illuminate\Http\JsonResponse Respuesta JSON con los detalles del estado.
+     */
     public function show(Request $request, int $id)
     {
         try
@@ -77,6 +121,14 @@ class StateController extends BaseController
         }
     }
 
+    /**
+     * Elimina un estado específico.
+     *
+     * Elimina un estado identificado por su ID y retorna una confirmación.
+     *
+     * @param int $id Identificador del estado a eliminar.
+     * @return \Illuminate\Http\JsonResponse Respuesta JSON de confirmación de eliminación.
+     */
     public function destroy(Request $request, int $id)
     {
         try
@@ -93,6 +145,13 @@ class StateController extends BaseController
         }
     }
 
+     /**
+     * Lista todos los estados disponibles.
+     *
+     * Retorna una lista de todos los estados existentes en la base de datos.
+     *
+     * @return \Illuminate\Http\JsonResponse Respuesta JSON con la lista de estados.
+     */
     public function index(Request $request)
     {
         try

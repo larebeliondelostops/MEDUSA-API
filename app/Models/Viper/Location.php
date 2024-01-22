768f9aba6487f93a49bc5258ee27453c724196f2
JorgeGorrito <j0rg3.4b3ll4@gmail.com>
2024-01-22 17:18:41 -0500

    Se creo de localizacion: migracion, interface basica, service basico

---
+++ app/Models/Viper/Location.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models\Viper;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Location extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'locations';
+
+    protected $dates = ['deleted_at'];
+
+    protected $fillable = [
+        'id',
+        'type',
+        'latitude',
+        'longitude'
+    ];
+}

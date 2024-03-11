3931a96d8daa9f8f6eda45046a8a9abaa180875d
danielxz331 <danielxz331@gmail.com>
2024-03-11 21:06:22 +0000

    Ajuste dinamismo Heatmap

--- app/Console/Commands/MovementUnits.php
+++ app/Console/Commands/MovementUnits.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Models\AvlHistoryCoordinates;
 use App\Models\Tenant;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 use Stancl\Tenancy\Concerns\TenantAwareCommand;
 
 class MovementUnits extends Command
@@ -27,7 +28,24 @@ class MovementUnits extends Command
 
     public function handle()
     {
-        $historicos = AvlHistoryCoordinates::all();
+        AvlHistoryCoordinates::orderBy('imei', 'desc')
+        ->chunk(40, function ($historicals) {
+            // Agrupa los registros del lote actual por 'imei'
+            foreach ($historicals as $historical)
+            {
+                $posiciones = json_decode($historical->position, true);
+
+                $firstElement = array_shift($posiciones);
+                array_push($posiciones, $firstElement);
+
+                $posiciones = json_encode($posiciones);
+
+                DB::table('avl_history_coordinates')
+                    ->where('imei', $historical->imei)
+                    ->update(['position' => $posiciones]);
+            }
+        });
+        /* $historicos = AvlHistoryCoordinates::all();
 
         foreach ($historicos as $historico)
         {
@@ -39,7 +57,7 @@ class MovementUnits extends Command
 
             $historico->position = json_encode($posiciones);
             $historico->save();
-        }
+        } */
     }
 
     protected function getTenants()

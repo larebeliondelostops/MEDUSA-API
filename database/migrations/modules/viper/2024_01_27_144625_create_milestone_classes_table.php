cca13700c3afeeee1f33bc3df94599a5ef305b14
danielxz331 <danielxz331@gmail.com>
2024-03-23 02:44:32 +0000

    correccion conflictos viper

--- database/migrations/modules/viper/2024_01_27_144625_create_milestone_classes_table.php
+++ database/migrations/modules/viper/2024_01_27_144625_create_milestone_classes_table.php
@@ -14,16 +14,9 @@ return new class extends Migration
      */
     public function up()
     {
-<<<<<<< HEAD:database/migrations/ditra/2023_07_19_204523_create_indicators_table.php
-        Schema::create('indicators', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('description');
-=======
         Schema::create('milestone_classes', function (Blueprint $table) {
             $table->id();
             $table->string('name',255);
->>>>>>> viper-develop:database/migrations/villavicencio/2024_01_27_144625_create_milestone_classes_table.php
             $table->timestamps();
         });
     }
@@ -35,10 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-<<<<<<< HEAD:database/migrations/ditra/2023_07_19_204523_create_indicators_table.php
-        Schema::dropIfExists('indicators');
-=======
         Schema::dropIfExists('milestone_class');
->>>>>>> viper-develop:database/migrations/villavicencio/2024_01_27_144625_create_milestone_classes_table.php
     }
 };

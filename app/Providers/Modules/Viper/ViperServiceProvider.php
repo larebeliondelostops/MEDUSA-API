f06c746db467857de14109889de9fcb45276a8ea
JorgeGorrito <j0rg3.4b3ll4@gmail.com>
2024-03-25 22:52:46 -0500

    Se mejoro la funcionalidad de actualizar deliverables al asignarles, actualizar, y eliminar una actividad

--- app/Providers/Modules/Viper/ViperServiceProvider.php
+++ app/Providers/Modules/Viper/ViperServiceProvider.php
@@ -5,7 +5,8 @@ namespace App\Providers\Modules\Viper;
 use App\Interfaces\Modules\Viper\ActivityInterface;
 use App\Interfaces\Modules\Viper\AlertInterface;
 use App\Interfaces\Modules\Viper\CoordinatesInterface;
-use App\Interfaces\Modules\Viper\DeliverableInterface;
+use App\Interfaces\Modules\Viper\Deliverable\DeliverableEventActivityInterface;
+use App\Interfaces\Modules\Viper\Deliverable\DeliverableInterface;
 use App\Interfaces\Modules\Viper\DepartmentInterface;
 use App\Interfaces\Modules\Viper\DocumentInterface;
 use App\Interfaces\Modules\Viper\FolderInterface;
@@ -19,7 +20,6 @@ use App\Interfaces\Modules\Viper\MunicipalityInterface;
 use App\Interfaces\Modules\Viper\PrecedenceInterface;
 use App\Interfaces\Modules\Viper\ProductInterface;
 use App\Interfaces\Modules\Viper\ProjectInterface;
-use App\Interfaces\Modules\Viper\ProjectMarkerInterface;
 use App\Interfaces\Modules\Viper\ProofInterface;
 use App\Interfaces\Modules\Viper\ReportInterface;
 use App\Interfaces\Modules\Viper\ScheduleInterface;
@@ -30,10 +30,12 @@ use App\Interfaces\Modules\Viper\SpecificObjectiveInterface;
 use App\Interfaces\Modules\Viper\StageInterface;
 use App\Interfaces\Modules\Viper\StateInterface;
 use App\Interfaces\Modules\Viper\SubstateInterface;
-use App\Services\Modules\Viper\ActivityService;
+use App\Models\Modules\Viper\Activity;
+use App\Services\Modules\Viper\Activity\ActivityService;
 use App\Services\Modules\Viper\AlertService;
 use App\Services\Modules\Viper\CoordinatesService;
-use App\Services\Modules\Viper\DeliverableService;
+use App\Services\Modules\Viper\Deliverable\DeliverableEventActivityService;
+use App\Services\Modules\Viper\Deliverable\DeliverableService;
 use App\Services\Modules\Viper\DepartmentService;
 use App\Services\Modules\Viper\DocumentService;
 use App\Services\Modules\Viper\FolderService;
@@ -46,7 +48,6 @@ use App\Services\Modules\Viper\MilestoneSubclassService;
 use App\Services\Modules\Viper\MunicipalityService;
 use App\Services\Modules\Viper\PrecedenceService;
 use App\Services\Modules\Viper\ProductService;
-use App\Services\Modules\Viper\ProjectMarkerService;
 use App\Services\Modules\Viper\ProjectService;
 use App\Services\Modules\Viper\ProofService;
 use App\Services\Modules\Viper\ReportService;
@@ -59,6 +60,7 @@ use App\Services\Modules\Viper\StageService;
 use App\Services\Modules\Viper\StateService;
 use App\Services\Modules\Viper\SubstateService;
 use Illuminate\Support\ServiceProvider;
+use App\Services\Modules\Viper\Activity\ActivityObserver;
 
 class ViperServiceProvider extends ServiceProvider
 {
@@ -81,7 +83,6 @@ class ViperServiceProvider extends ServiceProvider
         $this->app->bind(DeliverableInterface::class, DeliverableService::class);
         $this->app->bind(CoordinatesInterface::class, CoordinatesService::class);
         $this->app->bind(ProductInterface::class, ProductService::class);
-        $this->app->bind(ProjectMarkerInterface::class, ProjectMarkerService::class);
         $this->app->bind(ActivityInterface::class, ActivityService::class);
         $this->app->bind(PrecedenceInterface::class, PrecedenceService::class);
         $this->app->bind(LocationInterface::class, LocationService::class);
@@ -91,5 +92,12 @@ class ViperServiceProvider extends ServiceProvider
         $this->app->bind(ProofInterface::class, ProofService::class);
         $this->app->bind(ScheduleInterface::class, ScheduleService::class);
         $this->app->bind(ReportInterface::class, ReportService::class);
+        $this->app->bind(DeliverableEventActivityInterface::class, DeliverableEventActivityService::class);
+    }
+
+    public function boot()
+    {
+        // observers registered
+        Activity::observe(ActivityObserver::class);
     }
 }

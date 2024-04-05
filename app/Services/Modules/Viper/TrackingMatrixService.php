3e9d269fc449241bc08c95fc42b00a748f15bb12
JorgeGorrito <j0rg3.4b3ll4@gmail.com>
2024-04-04 19:59:01 -0500

    se mejoro la enumeracion de actividades y deliverables teniendo en cuenta el padre de cada uno

--- app/Services/Modules/Viper/TrackingMatrixService.php
+++ app/Services/Modules/Viper/TrackingMatrixService.php
@@ -14,32 +14,47 @@ class TrackingMatrixService implements TrackingMatrixInterface
     public function getTrackingMatrixOfProject(string $projectBpin) : Collection
     {
         $trackingMatrix = Project::join('scopes','scopes.project_id','=', 'projects.bpin')
-        ->join('specific_objectives','specific_objectives.scope_id','=','scopes.id')
-        ->select(['projects.bpin','projects.name'])
-        ->where('projects.bpin', $projectBpin)
-        ->with(['scope.specificObjectives.products.measurementUnit',
-            'scope.specificObjectives.products.deliverables' => function ($query) {
-                $query->whereNull('deliverable_id');
-            },
-            'scope.specificObjectives.products.reports',
-            'scope.specificObjectives.products.reports.proofs',
-            'scope.specificObjectives.products.deliverables.activities',
-            'scope.specificObjectives.products.deliverables.deliverables',
-            
-        ])
-        ->first();
-
+            ->join('specific_objectives','specific_objectives.scope_id','=','scopes.id')
+            ->select(['projects.bpin','projects.name'])
+            ->where('projects.bpin', $projectBpin)
+            ->with([
+                'scope.specificObjectives.products.measurementUnit',
+                'scope.specificObjectives.products.deliverables' => function ($query) {
+                    $query->whereNull('deliverable_id');
+                },
+                'scope.specificObjectives.products.deliverables.activities',
+                'scope.specificObjectives.products.deliverables.deliverables' => function ($query) {
+                    $query->with('activities');
+                }
+            ])
+            ->first();
+    
         if ($trackingMatrix && $trackingMatrix->scope && $trackingMatrix->scope->exists()) 
         {
-            $trackingMatrix->makeHidden(['bpin',]);
+            $trackingMatrix->makeHidden(['bpin']);
             $trackingMatrix->scope->makeHidden(['id', 'project_id']);
+            
             foreach($trackingMatrix->scope->specificObjectives as $specific_objective) 
             {
                 $specific_objective->makeHidden(['scope_id']);
+                
                 foreach($specific_objective->products as $product) 
                 {
                     $product->makeHidden(['measurement_unit_id', 'specific_objective_id', 'folder_id']);
                     $product->measurementUnit->makeHidden(['id']);
+    
+                    // Update deliverables number
+                    foreach ($product->deliverables as $index => $deliverable) {
+                        $deliverable->number = $product->number . '.' . ($index + 1);
+    
+                        // Update activities numbers
+                        foreach ($deliverable->activities as $activityIndex => $activity) {
+                            $activity->number = $deliverable->number . '.' . ($activityIndex + 1);
+                        }
+    
+                        // Recursively update numbers for child deliverables and activities
+                        $this->updateChildNumbers($deliverable, $deliverable->number);
+                    }
                 }
             }
             return collect($trackingMatrix);
@@ -50,4 +65,20 @@ class TrackingMatrixService implements TrackingMatrixInterface
         }
         return collect($trackingMatrix);
     }
+    
+    private function updateChildNumbers($parentDeliverable, $parentNumber)
+    {
+        foreach ($parentDeliverable->deliverables as $index => $childDeliverable) {
+            $childDeliverable->number = $parentNumber . '.' . ($index + 1);
+    
+            // Update activities numbers for child deliverable
+            foreach ($childDeliverable->activities as $activityIndex => $activity) {
+                $activity->number = $childDeliverable->number . '.' . ($activityIndex + 1);
+            }
+    
+            // Recursively update numbers for nested child deliverables and activities
+            $this->updateChildNumbers($childDeliverable, $childDeliverable->number);
+        }
+    }
+    
 }

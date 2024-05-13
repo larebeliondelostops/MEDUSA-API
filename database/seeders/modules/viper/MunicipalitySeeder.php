f2eb1ea86ae37e4517d273b711ffe3a9bb12b5df
JorgeGorrito <j0rg3.4b3ll4@gmail.com>
2024-05-13 17:52:28 -0500

    Se mejoro el seeder de municipios de villavicencio agregando mas municipios

--- database/seeders/modules/viper/MunicipalitySeeder.php
+++ database/seeders/modules/viper/MunicipalitySeeder.php
@@ -8,25 +8,35 @@ use Ramsey\Uuid\Uuid;
 
 class MunicipalitySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        /**
-         * Marcadores para villavicencio
-         */
         $coordinates = [
-            "Villavicencio" =>   [
-                            "id" => Uuid::uuid4()->toString(),
-                            "type" => "Point",
-                            "latitude" => 3.2219886587045397,
-                            "longitude" => -74.0877486836099,
-                            "created_at" => now(),
-                            "updated_at" => now(),
-                        ],
+            "Villavicencio" => [
+                "id" => Uuid::uuid4()->toString(),
+                "type" => "Point",
+                "latitude" => 3.2219886587045397,
+                "longitude" => -74.0877486836099,
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            // Agrega nuevos municipios aquí
+            "Acacías" => [
+                "id" => Uuid::uuid4()->toString(),
+                "type" => "Point",
+                "latitude" => 3.989037,
+                "longitude" => -73.757978,
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            "Granada" => [
+                "id" => Uuid::uuid4()->toString(),
+                "type" => "Point",
+                "latitude" => 3.546263,
+                "longitude" => -73.706879,
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            // Continúa agregando municipios...
         ];
 
         $municipalities = [
@@ -37,18 +47,31 @@ class MunicipalitySeeder extends Seeder
                 "created_at" => now(),
                 "updated_at" => now(),
             ],
+            // Agrega los nuevos municipios aquí
+            [
+                "name" => "Acacías",
+                "coordinate_id" => $coordinates["Acacías"]['id'],
+                "department_id" => 1,
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            [
+                "name" => "Granada",
+                "coordinate_id" => $coordinates["Granada"]['id'],
+                "department_id" => 1,
+                "created_at" => now(),
+                "updated_at" => now(),
+            ],
+            // Continúa agregando municipios...
         ];
 
-        //Primero se debe registrar la locacion
-        foreach ($coordinates as $coordinate)
-        DB::table('coordinates')->insert([
-            $coordinate
-        ]);
+        // Primero se debe registrar la locación
+        foreach ($coordinates as $coordinate) {
+            DB::table('coordinates')->insert($coordinate);
+        }
 
-        foreach ($municipalities as $municipality)
-            DB::table('municipalities')->insert([
-                $municipality
-            ]);
-
+        foreach ($municipalities as $municipality) {
+            DB::table('municipalities')->insert($municipality);
+        }
     }
 }

2ba46b4a34a583090cd896a6d23661ea8331b5f3
JorgeGorrito <j0rg3.4b3ll4@gmail.com>
2024-05-15 08:50:59 -0500

    Se actualizo el servicio de project para crear projectos con mga

--- app/Services/Modules/Viper/ProjectService.php
+++ app/Services/Modules/Viper/ProjectService.php
@@ -4,15 +4,28 @@ namespace App\Services\Modules\Viper;
 
 // Librerias del modulo viper
 
+use App\Interfaces\Modules\Viper\ActivityInterface;
+use App\Interfaces\Modules\Viper\Deliverable\DeliverableInterface;
+use App\Interfaces\Modules\Viper\DepartmentInterface;
+use App\Interfaces\Modules\Viper\FolderInterface;
+use App\Interfaces\Modules\Viper\IndicatorInterface;
 use App\Interfaces\Modules\Viper\LocationInterface;
+use App\Interfaces\Modules\Viper\MeasurementUnitInterface;
+use App\Interfaces\Modules\Viper\MunicipalityInterface;
+use App\Interfaces\Modules\Viper\ProductInterface;
 use App\Interfaces\Modules\Viper\ProjectInterface;
 use App\Models\Modules\Viper\Project;
 use App\Interfaces\Modules\Viper\ProjectMunicipalityInterface;
-
+use App\Interfaces\Modules\Viper\ScopeInterface;
+use App\Interfaces\Modules\Viper\SectorInterface;
+use App\Interfaces\Modules\Viper\SpecificObjectiveInterface;
+use App\Models\Modules\Viper\MeasurementUnit;
 // Librerias de terceros
 use App\Utils\Filters\Modules\Viper\ProjectFilter;
-use Illuminate\Http\Request;
+use Illuminate\Http\Client\RequestException;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Collection;
+use GuzzleHttp\Client;
 
 /**
  * Servicio para manejar operaciones relacionadas con proyectos.
@@ -29,14 +42,43 @@ class ProjectService implements ProjectInterface
 {
     private LocationInterface $locationInterface;
     private ProjectMunicipalityInterface $projectMunicipalityInterface;
+    private SectorInterface $sectorInterface;
+    private DepartmentInterface $departmentInterface;
+    private MunicipalityInterface $municipalityInterface;
+    private FolderInterface $folderInterface;
+    private ScopeInterface  $scopeInterface;
+    private SpecificObjectiveInterface $specificObjectiveInterface;
+    private ProductInterface $productInterface;
+    private DeliverableInterface $deliverableInterface;
+    private IndicatorInterface $indicatorInterface;
+
+    
 
     public function __construct(
         LocationInterface $locationInterface,
-        ProjectMunicipalityInterface $projectMunicipalityInterface
+        ProjectMunicipalityInterface $projectMunicipalityInterface,
+        SectorInterface $sectorInterface,
+        DepartmentInterface $departmentInterface,
+        MunicipalityInterface $municipalityInterface,
+        FolderInterface $folderInterface,
+        ScopeInterface $scopeInterface,
+        SpecificObjectiveInterface $specificObjectiveInterface,
+        ProductInterface $productInterface,
+        DeliverableInterface $deliverableInterface,
+        IndicatorInterface $indicatorInterface
     )
     {
         $this->locationInterface = $locationInterface;
         $this->projectMunicipalityInterface = $projectMunicipalityInterface;
+        $this->sectorInterface = $sectorInterface;
+        $this->departmentInterface = $departmentInterface;
+        $this->municipalityInterface = $municipalityInterface;
+        $this->folderInterface = $folderInterface;
+        $this->specificObjectiveInterface = $specificObjectiveInterface;
+        $this->scopeInterface = $scopeInterface;
+        $this->productInterface = $productInterface;
+        $this->deliverableInterface = $deliverableInterface;
+        $this->indicatorInterface = $indicatorInterface;
     }
 
     /**
@@ -130,13 +172,10 @@ class ProjectService implements ProjectInterface
         
        // Obtener los IDs de los municipios existentes y de los municipios del proyecto
         $existingMunicipalityIds = array_column($existingMunicipalities, 'id');
-        error_log("existingMunicipalityIds: ".json_encode($existingMunicipalityIds));
         $requestMunicipalityIds = array_column($requestMunicipalities, 'id');
-        error_log("requestMunicipalityIds: ".json_encode($requestMunicipalityIds));
 
         // Determinar los IDs de los municipios que se deben crear
         $municipalitiesIdsForCreate = array_values(array_diff($requestMunicipalityIds, $existingMunicipalityIds));
-        error_log("municipalitiesIdsForCreate: ".json_encode($municipalitiesIdsForCreate));
 
         // Iterar sobre los IDs de los municipios que se deben crear y crearlos
         foreach ($municipalitiesIdsForCreate as $municipalityId) {
@@ -159,7 +198,6 @@ class ProjectService implements ProjectInterface
 
         // Determinar los IDs de los municipios que se deben eliminar
         $municipalitiesIdsForDelete = array_values(array_diff($existingMunicipalityIds, $requestMunicipalityIds));
-        error_log("municipalitiesIdsForDelete: ".json_encode($municipalitiesIdsForDelete));
 
         // Iterar sobre los IDs de los municipios que se deben eliminar y eliminarlos
         foreach ($municipalitiesIdsForDelete as $municipalityId) {
@@ -267,7 +305,6 @@ class ProjectService implements ProjectInterface
         unset($projectData['project_municipality']);
 
         $projectData['total_value'] = (float)$projectData['total_value'];
-        $projectData['requested_value'] = (float)$projectData['requested_value'];
 
         foreach ($projectData['locations'] as $key => $location)
         {
@@ -292,5 +329,281 @@ class ProjectService implements ProjectInterface
         $projectDTO = $this->getProjectByBPIN($bpin);
         $project->delete();
         return $projectDTO;
+    }
+
+    /**
+     * Crea un nuevo proyecto a partir de un archivo MGA.
+     * 
+     * @param UploadedFile $mgaFile
+     * @return Collection
+     */
+    public function createNewProjectFromMGA(UploadedFile $mgaFile): Collection
+    {
+        try {
+            // Verifica si el archivo MGA es válido
+            if (!$mgaFile->isValid()) {
+                throw new \InvalidArgumentException('El archivo MGA no es válido.');
+            }
+
+            // Configura el cliente GuzzleHttp con la opción 'verify' establecida en false
+            $client = new Client([
+                'verify' => false, // Desactiva la verificación del certificado SSL
+            ]);
+
+            // Envía la solicitud POST al servicio externo con el archivo adjunto como form-data
+            $response = $client->request('POST', 'https://' . env('SCRAPINGDOCS_URL') . '/mga-extractor-information', [
+                'multipart' => [
+                    [
+                        'name' => 'mgaFile', // Nombre del campo en el formulario
+                        'contents' => fopen($mgaFile->path(), 'r'), // Contenido del archivo
+                        'filename' => $mgaFile->getClientOriginalName() // Nombre original del archivo
+                    ]
+                ]
+            ]);
+
+            // Verifica si la solicitud fue exitosa
+            if ($response->getStatusCode() !== 200) {
+                throw new \RuntimeException('Error al enviar el archivo MGA al servicio externo.');
+            }
+
+            $dataMga = json_decode($response->getBody()->getContents(), true)['mgaData'];
+            $municipalities = array_map(
+                function($municipality) {
+                    return ['municipality_id' => $this->municipalityInterface->getAllMunicipalities(["nameMunicipality" => ["eq" => $municipality]])->first()->id];
+                },
+                $dataMga['municipalities'][0]
+            );
+            $department_id = $this->departmentInterface->getAllDepartments(["nameDepartment" => ["eq" => $dataMga['department']]])->first()->id;
+            $sector_id = $this->sectorInterface->getAllSectors(["name" => ["eq" => $dataMga['sector']]])->first()->id;
+            
+            $project = $this->createNewProject(
+                collect([ 
+                    "bpin" => $dataMga['bpin'],
+                    "name" => $dataMga['name'],
+                    "ocad" => "",
+                    "state_id" => null,
+                    "substate_id" => null,
+                    "total_value" => $dataMga['total_value'],
+                    "responsible_entity" => "",
+                    "sector_id" => $sector_id,
+                    "department_id" => $department_id,
+                    "municipalities" => $municipalities,
+                    "locations" => [],
+                    "beneficiaries" => $dataMga['beneficiaries'],
+                    "planner" => $dataMga['planner'] ?? '',
+                    "execution_approval_date" => null,
+                    "completion_date" => null,
+                    "start_date_execution_phase" => null,
+                    "unilateral_termination" => null,
+                    "bilateral_termination" => null,
+                    "project_duration_in_months" => null,
+                    "reporting_frequency" => null,
+                ])
+            );
+            
+            // Se crean las carpetas
+            $structureFolder = '
+            {
+                "folders": [
+                     {
+                        "name": "Formulación",
+                        "stage_id": 1
+                    },
+                    {
+                        "name": "Presentación",
+                        "stage_id": 2,
+                        "subfolders": [
+                            {"name": "Proyecto en revisión"},
+                            {"name": "Fichas de revisión"}
+                        ]
+                    },
+                    {
+                        "name": "Viabilización",
+                        "stage_id": 3,
+                        "subfolders": [
+                            {"name": "Proyecto aprobado"},
+                            {
+                                "name": "Viabilización - Estructura del proyecto",
+                                "subfolders": [
+                                    {"name": "Aspectos técnicos"},
+                                    {"name": "Aspectos financieros"},
+                                    {"name": "Aspectos jurídicos"}
+                                ]
+                            }
+                        ]
+                    },
+                      {
+                        "name": "Programación",
+                        "stage_id": 4,
+                        "subfolders": [
+                            {
+                                "name": "Ejecutor del proyecto"
+                            },
+                            {
+                                "name": "Ajustes de programación"
+                            },
+                            {
+                                "name": "Gesproy",
+                                "subfolders": [
+                                    {"name": "Programación inicial"},
+                                    {"name": "Documentos soportes reprogramacion inicial"}
+                                ]
+                            },
+                            {
+                                "name": "Requisitos previos de ejecución",
+                                "subfolders": [
+                                    {"name": "Requisitos previos al cumplimiento de ejecución del proyecto"},
+                                    {"name": "Certificado de cumplimiento de requisitos previos de ejecución"}
+                                ]
+                            }
+                        ]
+                    },
+                      {
+                        "name": "Ejecución",
+                        "stage_id": 5,
+                        "subfolders": [
+                             {
+                                "name": "CARPETA GESPROY",
+                                "subfolders": [
+                                    {"name": "Reporte de Ejecución"}
+                                ]
+                            },
+                            {
+                                "name": "Contratos del proyecto",
+                                "subfolders": [
+                                    {
+                                        "name": "Interventoría",
+                                        "subfolders": [
+                                            {"name": "Ajustes de interventoría"},
+                                            {"name": "Precontractual de interventoría"},
+                                            {"name": "Contractual de interventoría"},
+                                            {"name": "Ejecución de interventoría"},
+                                            {"name": "Cierre de interventoría"}
+                                        ]
+                                    }
+                                ]
+                            },
+                            {
+                                "name": "Actuaciones de los órganos de control",
+                                "subfolders": [
+                                    {"name": "Contraloría general"},
+                                    {"name": "Procuraduría general"},
+                                    {"name": "Fiscalía general"},
+                                    {
+                                        "name": "DNP",
+                                        "subfolders": [
+                                            {"name": "Visita de seguimiento del proyecto"},
+                                            {"name": "Plan de acción y soportes de cumplimiento del mismo"},
+                                            {"name": "Procedimiento preventivo PAP"}
+                                        ]
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                     {
+                        "name": "Cierre",
+                        "stage_id": 6,
+                        "subfolders": [
+                            {"name": "Cierre de Proyecto"},
+                            {"name": "Evaluación Ejecutiva de resultados del proyecto"},
+                            {"name": "Ajustes de cierre"}
+                        ]
+                    },
+                    {
+                        "name": "Operación",
+                        "stage_id": 7
+                    }
+                ]
+            }            
+            ';
+
+            $foldersData = json_decode($structureFolder, true)['folders'];
+            foreach ($foldersData as $folderData) {
+                $this->folderInterface->createFolderHierarchy($folderData, $dataMga['bpin']);
+            }
+
+            // crea el alcance del proyecto
+            $scope = $this->scopeInterface->createNewScope(
+                collect([
+                    "project_id" => $dataMga['bpin'],
+                    "description" => $dataMga['scope']
+                ])
+            );
+
+            $folder_id = $this->folderInterface->getAllFolders($dataMga['bpin'], ['name' => ['cont' => '%Ejecución de interventoría%']])[0]['folder']['id'];
+            // crea los objetivos especificos
+            foreach($dataMga['specific_objectives'] as $specificObjective)
+            {
+                $specificObjectiveSaved = $this->specificObjectiveInterface->createNewSpecificObjective(
+                    collect([
+                        "description" => $specificObjective['description'],
+                        "scope_id" => $scope['id']
+                    ])
+                );
+                
+                foreach ($specificObjective['products'] as $product)
+                {
+                    $parts = explode('.', $product['number']);
+                    $number = intval(end($parts));
+                    $productForSave = collect([
+                        "name" => $product['description'],
+                        "number" => $number,
+                        "amount" => $product['amount'],
+                        "folder_id" => $folder_id,
+                        "specific_objective_id" => $specificObjectiveSaved['id'],
+                        "measurement_unit_id" => null,
+                    ]);
+                    $productSaved = $this->productInterface->storeProduct(
+                       $productForSave 
+                    );
+
+                    foreach ($product['activities'] as $activity)
+                    {
+                        $parts = explode('.', $activity['number']);
+                        $number = intval(end($parts));
+                        $this->deliverableInterface->createNewDeliverable(
+                            collect([
+                                'name' => $activity['description'],
+                                'number' => $number,
+                                'product_id' => $productSaved['id'],
+                                'deliverable_id' => null,
+                            ])
+                        );
+                    }
+                    
+                    foreach($product['indicators'] as $indicator)
+                    {
+                        $parts = explode('.', $indicator['number']);
+                        $number = intval(end($parts));
+                        $this->indicatorInterface->createNewIndicator(
+                            collect([
+                                'name' => $indicator['description'],
+                                'start_year_of_goal' => null,
+                                'end_year_goal' => null,
+                                'target_value' => 0,
+                                'progress' => 0.0,
+                                'percentage_completed' => 0.0,
+                                'is_main' => ($indicator['isMain'] === 'Si'? true: false),
+                                'product_id' => $productSaved['id'],
+                                'measurement_unit_id' => null,
+                            ])
+                        );
+                    }
+                }
+
+            }
+            
+
+            // Devuelve la información obtenida del servicio externo como una colección
+            return collect($dataMga);
+        } catch (RequestException $e) {
+            // Captura la excepción de la petición fallida y registra el motivo del fallo en el registro de errores
+            $errorMessage = $e->getMessage();
+            error_log("Error en la petición HTTP: $errorMessage");
+
+            // Lanza la excepción nuevamente para que pueda ser manejada en otro lugar si es necesario
+            throw $e;
+        }
     }
 }

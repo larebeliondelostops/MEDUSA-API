a6b523621ef085615c73f4f011b5bf645020544e
JhonFanor <jforjuela@unillanos>
2024-05-15 20:38:45 -0500

    Revertir cambio en projectMGA

--- app/Services/Modules/Viper/ProjectService.php
+++ app/Services/Modules/Viper/ProjectService.php
@@ -381,7 +381,7 @@ class ProjectService implements ProjectInterface
                     "bpin" => $dataMga['bpin'],
                     "name" => $dataMga['name'],
                     "ocad" => "",
-                    "state_id" => 4,
+                    "state_id" => null,
                     "substate_id" => null,
                     "total_value" => $dataMga['total_value'],
                     "responsible_entity" => "",
